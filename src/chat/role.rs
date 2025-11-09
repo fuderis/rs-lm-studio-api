20d808f75d17013583e6431372981db307fd68da
Bulat Sh. <synapdrake@ya.ru>
2025-11-09 18:48:10 +0500

    Added cmp methods for 'Role' enum

--- src/chat/role.rs
+++ src/chat/role.rs
@@ -10,3 +10,20 @@ pub enum Role {
     #[serde(rename = "assistant")]
     Assistant,
 }
+
+impl Role {
+    /// Returns true if it's the system role
+    pub fn is_system(&self) -> bool {
+        self == &Self::System
+    }
+
+    /// Returns true if it's the user role
+    pub fn is_user(&self) -> bool {
+        self == &Self::User
+    }
+
+    /// Returns true if it's the assistant role
+    pub fn is_assist(&self) -> bool {
+        self == &Self::Assistant
+    }
+}
